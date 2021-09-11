618f8bc25a0602d8ea43a68d76c22ec2dc84f853
TylerCYQ <90386368+TylerCYQ@users.noreply.github.com>
2021-09-11 15:23:10 +0800

    Update GameManager.cs

--- GameManager.cs
+++ GameManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-//Conclusive Win Condition for Cirucit 7
+//GameManager 
 
 public class GameManager : MonoBehaviour
 {
